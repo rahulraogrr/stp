3ea2e07d5469f277f943e92275bfa4626f284863
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2022-04-18 20:56:30 +0530

    Added CRE(Centralised Rule Engine) Service

---
+++ cre-service/src/test/java/com/example/cre/CreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
